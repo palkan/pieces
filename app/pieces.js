7213286229e64cb24a9008ff232133358821359c
palkan <dementiev@webils.ru>
2014-08-18 11:18:42 +0400

    add browserify support

---
+++ app/pieces.js
@@ -0,0 +1,8 @@
+'use strict'
+window.pi = require('./core')
+require('./components')
+require('./net')
+require('./resources')
+require('./controllers')
+require('./views')
+module.exports = window.pi
